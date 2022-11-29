4b7f928ccbbb3e3dfbef8848e4d04c15f6a720a5
Shrishti S <shrs6@publicisgroupe.net>
2022-11-29 20:50:20 +0530

    added

---
+++ src/main/java/jenkinsproject/Concate.java
@@ -0,0 +1,11 @@
+package jenkinsproject;
+
+public class Concate {
+	public static void main(String[] args) {
+		String firstname = "Shri";
+		String lastname = "shti";
+		System.out.println("Hello "+firstname+lastname);
+				
+	}
+
+}
